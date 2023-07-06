7e0b979435de741ce2481e71757223ce06e52458
Thomas Hareau <thomas.hareau@doctolib.com>
2023-07-06 15:36:35 +0200

    Update main.rb

--- lib/main.rb
+++ lib/main.rb
@@ -6,5 +6,9 @@ class GithubActionGemTest
   def self.bye
     puts 'Bye, Github Action Gem Test! without bug'
   end
+
+  def self.bye2
+    puts 'Bye, Github Action Gem Test 2! without bug'
+  end
 end
 
